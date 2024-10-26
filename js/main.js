ca156f8bb0d262b0308dc987794ac616933faf92
PaoloFabris98 <f98paolo@gmail.com>
2024-10-26 16:15:46 +0200

    feat:validator

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import * as populator from "./populator.js";
 import * as funct from "./funct.js";
 import * as obj from "./obj.js"
+import * as val from "./validator.js";
 
 const buttonSubmit = document.getElementById("submit");
 const nameField = document.getElementById("nameInput");
@@ -11,13 +12,6 @@ const textField = document.getElementById("textArea");
 const promField = document.getElementById("promCode");
 const privacyField = document.getElementById("privacy");
 
-const nameErr = document.getElementById("nameAllert");
-const surnameErr = document.getElementById("surnameAllert");
-const emailErr = document.getElementById("emailAllert");
-const jobErr = document.getElementById("jobAllert");
-const textErr = document.getElementById("textAllert");
-const promErr = document.getElementById("promAllert");
-const privacyErr = document.getElementById("privacyAllert");
 
 const finalPryce = document.getElementById("finalPryce");
 
@@ -43,7 +37,7 @@ buttonSubmit.addEventListener("click", function (event) {
   const prom = promField.value;
   const privacy = privacyField.checked;
 
-  let err = 0;
+  
 
   console.log(name);
   console.log(surname);
@@ -53,52 +47,7 @@ buttonSubmit.addEventListener("click", function (event) {
   console.log(prom);
   console.log(privacy);
 
-  if (!funct.isString(name) || name === "") {
-    err++;
-    populator.error(nameErr);
-  } else {
-    populator.removeAlert(nameErr);
-  }
-
-  if (!funct.isString(surname) || surname === "") {
-    err++;
-    populator.error(surnameErr);
-  } else {
-    populator.removeAlert(surnameErr);
-  }
-  if (!funct.isString(email) || email === "" || !funct.validateEmail(email)) {
-    err++;
-    populator.error(emailErr);
-  } else {
-    populator.removeAlert(emailErr);
-  }
-
-  if (job === "Seleziona il lavoro" || !funct.isString(job)) {
-    err++;
-    populator.error(jobErr);
-  } else {
-    populator.removeAlert(jobErr);
-  }
-
-  if (!funct.isString(text) || text === "") {
-    err++;
-    populator.error(textErr);
-  } else {
-    populator.removeAlert(textErr);
-  }
-
-  if (!funct.isString(prom)) {
-    err++;
-    populator.error(promErr);
-  } else {
-    populator.removeAlert(promErr);
-  }
-  if (!privacy) {
-    err++;
-    populator.error(privacyErr);
-  } else {
-    populator.removeAlert(privacyErr);
-  }
+  val.validation(name,surname,email,job,text,prom,privacy)
 
   if (!(err != 0)) {
     let price = Number(0);
---
+++ js/validator.js
@@ -0,0 +1,59 @@
+import * as populator from "./populator.js";
+import * as funct from "./funct.js";
+const nameErr = document.getElementById("nameAllert");
+const surnameErr = document.getElementById("surnameAllert");
+const emailErr = document.getElementById("emailAllert");
+const jobErr = document.getElementById("jobAllert");
+const textErr = document.getElementById("textAllert");
+const promErr = document.getElementById("promAllert");
+const privacyErr = document.getElementById("privacyAllert");
+
+export function validation(name, surname, email, job, text, prom, privacy) {
+  let err = 0;
+  if (!funct.isString(name) || name === "") {
+    err++;
+    populator.error(nameErr);
+  } else {
+    populator.removeAlert(nameErr);
+  }
+
+  if (!funct.isString(surname) || surname === "") {
+    err++;
+    populator.error(surnameErr);
+  } else {
+    populator.removeAlert(surnameErr);
+  }
+  if (!funct.isString(email) || email === "" || !funct.validateEmail(email)) {
+    err++;
+    populator.error(emailErr);
+  } else {
+    populator.removeAlert(emailErr);
+  }
+
+  if (job === "Seleziona il lavoro" || !funct.isString(job)) {
+    err++;
+    populator.error(jobErr);
+  } else {
+    populator.removeAlert(jobErr);
+  }
+
+  if (!funct.isString(text) || text === "") {
+    err++;
+    populator.error(textErr);
+  } else {
+    populator.removeAlert(textErr);
+  }
+
+  if (!funct.isString(prom)) {
+    err++;
+    populator.error(promErr);
+  } else {
+    populator.removeAlert(promErr);
+  }
+  if (!privacy) {
+    err++;
+    populator.error(privacyErr);
+  } else {
+    populator.removeAlert(privacyErr);
+  }
+}
